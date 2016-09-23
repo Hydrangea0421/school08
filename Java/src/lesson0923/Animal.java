edc0334fed98dd3c353fa71accc0f2ff3309e0e8
Hydrangea <hydrangea19920421@yahoo.co.jp>
2016-09-23 12:56:33 +0900

    tsts

---
+++ Java/src/lesson0923/Animal.java
@@ -0,0 +1,9 @@
+package lesson0923;
+
+public class Animal {
+	protected int age;
+
+	Animal(int age) {
+		this.age = age;
+	}
+}
---
+++ Java/src/lesson0923/Dog.java
@@ -0,0 +1,16 @@
+package lesson0923;
+
+public class Dog extends Animal {
+	private String name;
+
+	public Dog(int age, String name) {
+		super(age);
+		this.name = name;
+	}
+
+	void say(){
+		System.out.println(age +"才の"+ name +"が鳴きました");
+	}
+
+
+}
